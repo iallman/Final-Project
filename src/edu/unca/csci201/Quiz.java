9b33b222d3f8688cb8c7a64ce9b11ade27731406
KH037s <KH037s@KH037-02.uncanet.unca.edu>
2018-04-11 18:50:23 -0400

    initial commit

---
+++ src/edu/unca/csci201/Quiz.java
@@ -0,0 +1,20 @@
+package edu.unca.csci201;
+
+public class Quiz {
+
+	
+	
+	public void addQuestion(Question q) {
+		
+		Object[] listOfQuestions = new Question [25];
+		
+	}
+	
+	public double giveQuiz() {
+		
+		
+		return double;
+		
+	}
+
+}
